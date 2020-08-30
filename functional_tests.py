105704480391b9a642edb40522fc63f176c61f8f
Oscar <oxf848@cs.bham.ac.uk>
2020-08-30 15:42:34 +0100

    functional tests now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
-print ('working')
-browser = webdriver.Firefox()
-print ('working')
-browser.get('http://127.0.0.1:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish the test!')
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
